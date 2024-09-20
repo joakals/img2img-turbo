7423caaf0ef3ca07f937389ef78a8caccb1309c1
joakim <joakim@icgi.no>
2024-09-20 14:07:21 +0200

    Add Dockerfile

---
+++ run_cycleganturbo-training.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export NCCL_P2P_DISABLE=1
+
+accelerate launch --main_process_port 29501 --config_file accelerate_configs/config_8gpu_12GB.sh \
+    src/train_cyclegan_turbo.py \
+    --pretrained_model_name_or_path="stabilityai/sd-turbo" \
+    --output_dir="output/cyclegan_turbo/my_horse2zebra" \
+    --dataset_folder "data/my_horse2zebra" \
+    --train_img_prep "resize_286_randomcrop_56x56_hflip" --val_img_prep "no_resize" \
+    --learning_rate="1e-5" --max_train_steps=25000 \
+    --train_batch_size=1 --gradient_accumulation_steps=1 \
+    --report_to "wandb" --tracker_project_name "gparmar_unpaired_h2z_cycle_debug_v3" \
+    --enable_xformers_memory_efficient_attention --validation_steps 250 \
+    --lambda_gan 0.5 --lambda_idt 1 --lambda_cycle 1
